223c6f2251676f584ed88c629420b44ed6fdfa80
yasinldev <ozkayayasin964@gmail.com>
2023-09-07 00:03:52 +0300

    Update: Added exception

--- src/commands/create/create.rs
+++ src/commands/create/create.rs
@@ -5,10 +5,16 @@ use flate2::write::GzEncoder;
 use std::error::Error;
 use colored::*;
 
+use crate::exceptions::exceptions::{throw_gel_global_exception, GelExceptions};
+
+//pub(crate) fn check_deps(_args: String) -> Result
+
 pub(crate) fn handle_create(_arg: String) -> Result<(), Box<dyn Error>> {
     if _arg == "" || _arg == "." {
         eprintln!("{} {}", "Error: Please specify a folder name".red(), "gel create <folder_name>".red());
-        std::process::exit(1)
+        throw_gel_global_exception(
+            GelExceptions::GelGlobalExceptions("undefined folder name (no such file or directory)".to_string())
+        );
     }
     let folder_name = _arg;
     let gel = File::create(format!("{}.gel", folder_name))?;
